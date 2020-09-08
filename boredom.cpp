d6bca1c5b4668587b7f8e65478acea2c52527648
Akhil Sharma <sharma.akhil484@gmail.com>
2020-09-08 11:58:22 +0530

    Add files via upload

---
+++ boredom.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+	int n;
+	cin>>n;
+	int a[n];
+	for(int i=0;i<n;i++)
+		cin>>a[i];
+
+	long long c[100005];
+	long long dp[100005];
+	memset(c,0,sizeof(c));
+	for(int i=0;i<n;i++)
+		c[a[i]]++;
+	dp[0] = 0;
+	dp[1] = c[1]*1;
+	for(int i=2;i<=100000;i++)
+	{
+		dp[i] = max(dp[i-1],i*c[i]+dp[i-2]);		//if one chooses i then i-1 will be removed and if one chooses i-1 then i will be removed
+
+	}
+	cout<<dp[100000];
+}
