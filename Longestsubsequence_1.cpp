a09638783b595bcdd7b440fc0bf25b1b3442f21b
Akhil Sharma <sharma.akhil484@gmail.com>
2020-10-07 15:20:59 +0530

    Create Longestsubsequence_1.cpp

---
+++ Longestsubsequence_1.cpp
@@ -0,0 +1,59 @@
+// { Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+
+// User function Template for C++
+
+class Solution{
+public:
+    int longestSubsequence(int N, int A[])
+    {
+        // code here
+        int dp[N];
+        int m = INT_MIN;
+        
+        for(int i=0;i<N;i++)
+        {
+            dp[i]=1;
+        }
+        for(int i=1;i<N;i++)
+        {
+            for(int j=0;j<i;j++)
+            {
+                if(abs(A[i]-A[j])==1)
+                {
+                    dp[i] = max(dp[i],dp[j] +1);
+                }
+            }
+        }
+        for(int i=0;i<N;i++)
+        {
+            if(m<dp[i])
+                m=dp[i];
+        }
+        return m;
+    }
+};
+
+// { Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        int A[N];
+        for(int i = 0;i < N;i++)
+            cin>>A[i];
+        
+        Solution ob;
+        cout<<ob.longestSubsequence(N, A)<<endl;
+    }
+    return 0;
+}  // } Driver Code Ends
