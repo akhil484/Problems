8eab9c7cecd242e6434fa89bcdaf3749a2fd1ad6
Akhil Sharma <sharma.akhil484@gmail.com>
2020-10-24 23:02:03 +0530

    Update BurstBallons.cpp

--- BurstBallons.cpp
+++ BurstBallons.cpp
@@ -13,8 +13,8 @@ public:
                 dp[i][j]=INT_MIN;
                 for(int k=i;k<=j;k++)
                 {
-                    int left = (k==i)? 0:dp[i][k-1];
-                    int right = (k==j)? 0:dp[k+1][j];
+                    int left = (k==i)? 0:dp[i][k-1];    //if k is starting of an array then there is nothing before it.
+                    int right = (k==j)? 0:dp[k+1][j];   //if k is end of array then there is nothing after it.
                     int val = (i==0?1:nums[i-1]) * nums[k] * (j==n-1?1:nums[j+1]);
                     int total = left + right + val;
                     if(total>dp[i][j])
