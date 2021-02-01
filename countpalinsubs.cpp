053ccc7bd74d06492c507f00e789212c72b786c5
Akhil Sharma <sharma.akhil484@gmail.com>
2021-02-01 12:54:22 +0530

    Create countpalinsubs.cpp

---
+++ countpalinsubs.cpp
@@ -0,0 +1,51 @@
+// { Driver Code Starts
+// Counts Palindromic Subsequence in a given String
+#include<iostream>
+#include<cstring>
+using namespace std;
+ 
+// Function return the total palindromic subsequence
+int countPS(string str);
+ 
+// Driver program
+int main()
+{
+   int t;
+	cin>>t;
+   while(t--)
+	{
+	string str;
+cin>>str;
+cout<<countPS(str)<<endl;
+} 
+}// } Driver Code Ends
+
+
+/*You are required to complete below method */
+int countPS(string str)
+{
+   //Your code here
+   int n=str.length();
+   int dp[2][n];
+   memset(dp,0,sizeof(dp));
+   
+   for(int i=n-1;i>=0;i--)
+   {
+       for(int j=i;j<n;j++)
+       {
+           if(i==j)
+            dp[i%2][j]=1;
+           else if(str[i]==str[j])
+           {
+               dp[i%2][j] = 1+ dp[1-i%2][j] + dp[i%2][j-1];
+           }
+           else
+           {
+               dp[i%2][j] = dp[1-i%2][j] + dp[i%2][j-1] - dp[1-i%2][j-1];
+           }
+            
+       }
+   }
+   return dp[0][n-1];
+}
+ 
