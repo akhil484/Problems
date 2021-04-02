6c7d7612e4085250f78b369a9b63300c0e7cded8
Akhil Sharma <sharma.akhil484@gmail.com>
2021-04-02 18:54:21 +0530

    Update nextPer.cpp

--- nextPer.cpp
+++ nextPer.cpp
@@ -2,20 +2,24 @@ class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
         
-        int n=nums.size();
-        if(n<=1)
-            return;
-        int i=n-2;
-        while(i>=0 && nums[i]>=nums[i+1])
-            i--;
-        if(i>=0)
-        {
-            int j=n-1;
-            while(nums[j]<=nums[i])
-                j--;
-            swap(nums[i],nums[j]);
+        int n = nums.size(), k, l;
+    	for (k = n - 2; k >= 0; k--) {
+            if (nums[k] < nums[k + 1]) {
+                break;
+            }
         }
-        reverse(nums.begin()+i+1,nums.end());
+    	if (k < 0) {
+    	    reverse(nums.begin(), nums.end());
+    	} else {
+    	    for (l = n - 1; l > k; l--) {
+                if (nums[l] > nums[k]) {
+                    break;
+                }
+            } 
+    	    swap(nums[k], nums[l]);
+    	    reverse(nums.begin() + k + 1, nums.end());
+        }
+       
         
     }
 
