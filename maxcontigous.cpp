ed1cd4b2840e283dd4f366c9a78714b47348059c
Akhil Sharma <sharma.akhil484@gmail.com>
2021-05-23 12:38:29 +0530

    Update maxcontigous.cpp

--- maxcontigous.cpp
+++ maxcontigous.cpp
@@ -18,11 +18,17 @@ public:
          vector<int> result;
     if (k == 0) return result;
     deque<int> w;
+        // The element at the front of the queue is the largest element of
+        // previous window, so add to result.
     for (int i = 0, n = (int)nums.size(); i < n; i++) {
+        // Remove the elements which are out of this window
         while (!w.empty() && w.front() <= i-k)
             w.pop_front();
+        // Remove all elements smaller than the currently
+        // being added element (remove useless elements)
         while (!w.empty() && nums[w.back()] <= nums[i])
             w.pop_back();
+        // Add current element at the rear of deque
         w.push_back(i);
         if (i >= k-1)
             result.push_back(nums[w.front()]);
