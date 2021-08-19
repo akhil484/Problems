9e8e68ddd4a5bba0ac4c7947256585ebee176040
Akhil Sharma <sharma.akhil484@gmail.com>
2021-08-19 14:45:46 +0530

    Update medianMatrix.cpp

--- medianMatrix.cpp
+++ medianMatrix.cpp
@@ -1,61 +1,30 @@
-// { Driver Code Starts
-//Initial template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
- // } Driver Code Ends
-
-
-//User function template for C++
-
-class Solution{   
-public:
-    int median(vector<vector<int>> &matrix, int r, int c){
-        // code here    
-        int mi=INT_MAX,m=INT_MIN;
-        for(int i=0;i<r;i++)
-        {
-            if(matrix[i][0]<mi)
-                mi=matrix[i][0];
-            if(matrix[i][c-1]>m)
-                m=matrix[i][c-1];
+int countSmallerThanMid(vector<int> &row, int mid) {
+    int l = 0, h = row.size() - 1; 
+    while(l <= h) {
+        int md = (l + h) >> 1; 
+        if(row[md] <= mid) {
+            l = md + 1;
         }
-        int des = (r*c+1)/2;
-        while(mi<m)
-        {
-            int mid = mi + (m-mi)/2;
-            int place = 0;
-            for(int i=0;i<r;i++)
-            {
-                place += upper_bound(matrix[i].begin(),matrix[i].end(),mid)-matrix[i].begin();
-            }
-            if(place<des)
-                mi=mid+1;
-            else
-                m=mid;
+        else {
+            h = md - 1;
         }
-        return m;
     }
-};
-
-// { Driver Code Starts.
-
-int main()
-{
-    int t;
-    cin>>t;
-    while(t--)
-    {
-        int r, c;
-        cin>>r>>c;
-        vector<vector<int>> matrix(r, vector<int>(c));
-        for(int i = 0; i < r; ++i)
-            for(int j = 0;j < c; ++j)
-                cin>>matrix[i][j];
-        Solution obj;
-        cout<<obj.median(matrix, r, c)<<endl;        
+    return l; 
+}
+int Solution::findMedian(vector<vector<int> > &A) {
+    int low = INT_MIN;
+    int high = INT_MAX; 
+    int n = A.size();
+    int m = A[0].size(); 
+    while(low <= high) {
+        int mid = (low + high) >> 1; 
+        int cnt = 0;
+        for(int i = 0;i<n;i++) {
+            cnt += countSmallerThanMid(A[i], mid); 
+        }
+        
+        if(cnt <= (n * m) / 2) low = mid + 1; 
+        else high = mid - 1; 
     }
-    return 0;
-}  // } Driver Code Ends
+    return low; 
+}
