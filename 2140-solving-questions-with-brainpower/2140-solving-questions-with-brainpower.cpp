ba565a1933c5a04e7ec07cbb30a74a9295275d9e
Akhil Sharma <sharma.akhil484@gmail.com>
2022-01-16 15:02:44 +0530

    Time: 555 ms (30.00%), Space: 115.3 MB (70.00%) - LeetHub

--- 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
@@ -3,6 +3,12 @@ public:
     long long mostPoints(vector<vector<int>>& questions) {
         int n = questions.size();
         vector<long long> dp(n+1,0);
+        // dp[i] means maximum points we can get at index i.
+        // we will have two choices at index i -
+        // (a) Include the current index-
+        // dp[i] = include current index points + dp[next index after skipping question[i][1] elements]
+        // (b)Skip this index-
+        // maximum points we can get for dp[i + 1]
         for(int i=n-1;i>=0;i--)
         {
             int points = questions[i][0];
