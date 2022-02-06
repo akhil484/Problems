2246e1fd3aa5cbacf97a8807c2436a0dcfeab8e2
Akhil Sharma <sharma.akhil484@gmail.com>
2022-02-06 23:42:09 +0530

    Time: 223 ms (100.00%), Space: 53.3 MB (88.89%) - LeetHub

--- 2167-minimum-time-to-remove-all-cars-containing-illegal-goods/2167-minimum-time-to-remove-all-cars-containing-illegal-goods.cpp
+++ 2167-minimum-time-to-remove-all-cars-containing-illegal-goods/2167-minimum-time-to-remove-all-cars-containing-illegal-goods.cpp
@@ -2,22 +2,40 @@ class Solution {
 public:
     int minimumTime(string s) {
         int n = s.length();
-        long long res=n;
-        vector<int> v;
-        for(auto it:s)
+        // long long res=n;
+        // vector<int> v;
+        // for(auto it:s)
+        // {
+        //     if(it=='0')
+        //         v.push_back(-1);
+        //     else
+        //         v.push_back(1);
+        // }
+        // int mini=0;
+        // int sum=0;
+        // for(auto it:v)
+        // {
+        //     sum = min(it,sum+it);
+        //     mini=min(mini,sum);
+        // }
+        // return n+mini;
+        vector<int> dp(n,0);
+        dp[n-1] = s[n-1]=='0'?0:1;
+        for(int i=n-2;i>=0;i--)
         {
-            if(it=='0')
-                v.push_back(-1);
+            if(s[i]=='0')
+                dp[i]=dp[i+1];
             else
-                v.push_back(1);
+            {
+                dp[i]=2+dp[i+1];
+                dp[i]=min(dp[i],n-i); 
+            }
         }
-        int mini=0;
-        int sum=0;
-        for(auto it:v)
-        {
-            sum = min(it,sum+it);
-            mini=min(mini,sum);
+        int ans=dp[0];
+        for(int i=0;i<n-1;i++){
+            ans=min(ans,i+1+dp[i+1]); 
         }
-        return n+mini;
+        ans=min(ans,n); 
+        return ans;
     }
 };
