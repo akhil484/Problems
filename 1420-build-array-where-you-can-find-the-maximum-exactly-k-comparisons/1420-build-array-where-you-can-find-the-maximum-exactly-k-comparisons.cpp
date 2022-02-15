cfc606b78f160e03dffd6bd386aa3ab2712b9a22
Akhil Sharma <sharma.akhil484@gmail.com>
2022-02-15 12:57:16 +0530

    Time: 16 ms (86.41%), Space: 10.4 MB (24.46%) - LeetHub

--- 1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons.cpp
+++ 1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
     long long dp[51][101][51];
+    long long pre[51][101][51];
     const int MOD = 1e9 + 7;
     int numOfArrays(int n, int m, int k) {
      //dp[i][j][k] is ways to create array of length i with max element j and cost k;
         for(int j=1;j<=m;j++)
         {
             dp[1][j][1] =1;
+            pre[1][j][1] =1;
         }
         for(int i=1;i<=n;i++)
         {
@@ -16,9 +18,11 @@ public:
                 {
                     long long s = 0;
                     s = s+(j*dp[i-1][j][z])%MOD;
-                    for(int x=1;x<j;x++)
-                        s=(s+dp[i-1][x][z-1])%MOD;
+                    // for(int x=1;x<j;x++)
+                    //     s=(s+dp[i-1][x][z-1])%MOD;
+                    s = (s+pre[i-1][j-1][z-1])%MOD;
                     dp[i][j][z] = dp[i][j][z]+s;
+                    pre[i][j][z] = (pre[i][j-1][z]+dp[i][j][z])%MOD;
                 }
             }
         }
