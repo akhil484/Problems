bf8ddcb5f86eca067d47e6591dd8e6e6e19151f7
Akhil Sharma <sharma.akhil484@gmail.com>
2022-02-17 15:10:03 +0530

    Time: 142 ms (69.93%), Space: 39.6 MB (55.13%) - LeetHub

---
+++ 2088-count-fertile-pyramids-in-a-land/2088-count-fertile-pyramids-in-a-land.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int count(vector<vector<int>> grid)
+    {
+        int n=grid.size();
+        int m=grid[0].size();
+        int ans=0;
+        for(int i=1;i<n;i++)
+        {
+            for(int j=1;j<m-1;j++)
+            {
+                if(grid[i][j]&&grid[i-1][j])
+                {
+                    grid[i][j] = min(grid[i-1][j-1],grid[i-1][j+1])+1;
+                    ans+=grid[i][j]-1;
+                }
+            }
+        }
+        return ans;
+    }
+    int countPyramids(vector<vector<int>>& grid) {
+        int ans = count(grid); 
+        reverse(grid.begin(), grid.end());  
+        ans += count(grid);
+        return ans;
+    }
+};
