5e2332560b082f0b2dbd5542e8aa44032e119ddf
Akhil Sharma <sharma.akhil484@gmail.com>
2022-03-04 14:57:26 +0530

    Time: 9 ms (82.07%), Space: 9.1 MB (44.68%) - LeetHub

--- 1320-minimum-distance-to-type-a-word-using-two-fingers/1320-minimum-distance-to-type-a-word-using-two-fingers.cpp
+++ 1320-minimum-distance-to-type-a-word-using-two-fingers/1320-minimum-distance-to-type-a-word-using-two-fingers.cpp
@@ -1,28 +1,27 @@
 class Solution {
 public:
-    const int alpha = 27;
-    const int inf = 301;
     
-    int cost(char from, char to)
+    
+    int cost(int from, int to)
     {
         return from==26?0:abs(from/6-to/6)+abs(from%6-to%6);
     }
     
-    int calculate(string &word, int pos, char other,vector<vector<int>> &dp)
+    int calculate(string &word, int pos, int other,vector<vector<int>> &dp)
     {
         if(pos>=word.size())
             return 0;
         if(dp[other][pos]==-1)
         {
-            auto to = word[pos]-'A';
-            auto last = word[pos-1]-'A';
+            int to = word[pos]-'A';
+            int last = word[pos-1]-'A';
             dp[other][pos] = min(cost(other,to)+calculate(word,pos+1,last,dp),cost(last,to)+calculate(word,pos+1,other,dp));
         }
         return dp[other][pos];
     }
     
     int minimumDistance(string word) {
-        vector<vector<int>> dp(alpha,vector<int>(inf,-1));
+        vector<vector<int>> dp(27,vector<int>(301,-1));
         return calculate(word,1,26,dp);
     }
 };
