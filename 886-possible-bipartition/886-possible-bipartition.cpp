7a9177cc6b9178a8c41c99c6f7b53ca874180b22
Akhil Sharma <sharma.akhil484@gmail.com>
2022-03-08 13:27:14 +0530

    Time: 360 ms (29.46%), Space: 65.4 MB (55.81%) - LeetHub

--- 886-possible-bipartition/886-possible-bipartition.cpp
+++ 886-possible-bipartition/886-possible-bipartition.cpp
@@ -1,5 +1,39 @@
 class Solution {
 public:
+    bool dfs(int node, vector<vector<int>> &graph, vector<int> &color)
+    {
+        if(color[node]==-1)
+            color[node]=1;
+        for(auto it:graph[node])
+        {
+            if(color[it]==-1)
+            {
+                color[it]=1-color[node];
+                if(!dfs(it,graph,color))
+                    return false;
+            }
+            else
+            {
+                if(color[it]==color[node])
+                    return false;
+            }
+        }
+        return true;
+    }
+    
+    bool check(vector<vector<int>> &graph, int n)
+    {
+        vector<int> color(n+1,-1);
+        for(int i=1;i<=n;i++)
+        {
+            if(color[i]==-1){
+            if(!dfs(i,graph,color))
+                return false;
+            }
+        }
+        return true;
+    }
+    
     bool possibleBipartition(int n, vector<vector<int>>& dislikes) {
         vector<int> color(n+1,-1);
         vector<int> vis(n+1,0);
@@ -9,34 +43,38 @@ public:
             graph[dislikes[i][0]].push_back(dislikes[i][1]);
             graph[dislikes[i][1]].push_back(dislikes[i][0]);
         }
-        queue<int> q;
+//         queue<int> q;
         
-        for(int i=1;i<=n;i++)
-        {
-            if(vis[i]==0)
-            {
-                color[i]=0;
-                q.push(i);
-                while(!q.empty())
-                {
-                    int u = q.front();
-                    q.pop();
-                    if(vis[u]==1)
-                        continue;
-                    vis[u]=1;
-                    for(int v:graph[u])
-                    {
-                        if(color[v]==color[u])
-                            return false;
-                        if(color[u]==0)
-                            color[v]=1;
-                        else
-                            color[v]=0;
-                        q.push(v);
-                    }
-                }
-            }
-        }
-        return true;
+//         for(int i=1;i<=n;i++)
+//         {
+//             if(vis[i]==0)
+//             {
+//                 color[i]=0;
+//                 q.push(i);
+//                 while(!q.empty())
+//                 {
+//                     int u = q.front();
+//                     q.pop();
+//                     if(vis[u]==1)
+//                         continue;
+//                     vis[u]=1;
+//                     for(int v:graph[u])
+//                     {
+//                         if(color[v]==color[u])
+//                             return false;
+//                         if(color[u]==0)
+//                             color[v]=1;
+//                         else
+//                             color[v]=0;
+//                         q.push(v);
+//                     }
+//                 }
+//             }
+//         }
+//         return true;
+        if(check(graph,n))
+            return true;
+        else
+            return false;
     }
 };
