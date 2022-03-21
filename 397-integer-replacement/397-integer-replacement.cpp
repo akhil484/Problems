105e39a1d8adfeeeaff66ea7c2a5643fa7504140
Akhil Sharma <sharma.akhil484@gmail.com>
2022-03-21 11:17:32 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (83.66%) - LeetHub

--- 397-integer-replacement/397-integer-replacement.cpp
+++ 397-integer-replacement/397-integer-replacement.cpp
@@ -2,23 +2,15 @@ class Solution {
 public:
     
     int integerReplacement(int n) {
-        int ans=0;
-        while(n!=1)
-        {
-            if(n==3)
-                return ans+=2;
-             if (n == INT_MAX) return 32;
-            else if(n%2==0)
-            {
-                n=n/2;
-            }
-            else if((n+1)%4==0)
-                n++;
-            else
-                n--;
-            ans++;
-            
-        }
-        return ans;
+        if(n==INT_MAX)
+            return 32;
+        if(n==1)
+            return 0;
+        if(n==3)
+            return 2;
+        if(n&1)
+            return n%4==3?1+integerReplacement(n+1):1+integerReplacement(n-1);
+        else
+            return 1+integerReplacement(n/2);
     }
 };
