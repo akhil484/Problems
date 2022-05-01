7b9eda48b64790422fe106b177377e77ddd871e7
Akhil Sharma <sharma.akhil484@gmail.com>
2022-05-01 12:46:02 +0530

    Time: 72 ms (100.00%), Space: 10.6 MB (75.00%) - LeetHub

---
+++ 2262-total-appeal-of-a-string/2262-total-appeal-of-a-string.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    long long appealSum(string s) {
+        vector<int> last(26,0);
+        int n = s.length();
+        long long res=0;
+        for(int i=0;i<n;i++)
+        {
+            last[s[i]-'a']=i+1;
+            //count all substrings ending at i which contain charater l;
+            for(int l: last)
+                res+=l;
+        }
+        return res;
+    }
+};
