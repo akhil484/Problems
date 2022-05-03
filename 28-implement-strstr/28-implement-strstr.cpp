0b048de0b6ad87152f05a486a8d1a308d7efd122
Akhil Sharma <sharma.akhil484@gmail.com>
2022-05-03 16:46:26 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (64.36%) - LeetHub

---
+++ 28-implement-strstr/28-implement-strstr.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    vector<int> calcP(string needle)
+    {
+        int n = needle.length();
+        vector<int> res(n,0);
+        int i=1,j=0;
+        while(i<n)
+        {
+            if(needle[i]==needle[j])
+            {
+                res[i]=j+1;
+                i++;
+                j++;
+            }
+            else
+            {
+                if(j!=0)
+                {
+                    j=res[j-1];
+                }
+                else
+                {
+                    res[i]=0;
+                    i++;
+                }
+            }
+        }
+        return res;
+    }
+    
+    int strStr(string haystack, string needle) {
+        vector<int> temp = calcP(needle);
+        int n = haystack.length();
+        int i=0,j=0;
+        while(i<n&&j<needle.length())
+        {
+            if(haystack[i]==needle[j])
+            {
+                i++;
+                j++;
+            }
+            else
+            {
+                if(j!=0)
+                    j=temp[j-1];
+                else
+                    i++;
+            }
+        }
+        if(j==needle.length())
+            return i-j;
+        return -1;
+    }
+};
