e9f36f636f6bd12429d6bc4f04a98db92314b87c
Akhil Sharma <sharma.akhil484@gmail.com>
2022-05-04 21:24:13 +0530

    Time: 194 ms (5.14%), Space: 69.4 MB (52.76%) - LeetHub

--- 134-gas-station/134-gas-station.cpp
+++ 134-gas-station/134-gas-station.cpp
@@ -1,3 +1,7 @@
+//total sum should be greater than or equal to 0.
+//Let us say at i I am not able to go further that means from 0 to i there will be no solution as 
+//the total sum for them is negative and to compensate that from i+1to end should be net positive and greater than or equal to the negative part.
+
 class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
