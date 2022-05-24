f5b768d84d82855e760fc60665554f69ff0d4549
Akhil Sharma <sharma.akhil484@gmail.com>
2022-05-24 22:46:55 +0530

    Time: 4 ms (37.65%), Space: 5.9 MB (52.35%) - LeetHub

---
+++ 1611-minimum-one-bit-operations-to-make-integers-zero/1611-minimum-one-bit-operations-to-make-integers-zero.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minimumOneBitOperations(int n) {
+        //It takes 2^n - 1 steps of operation for number 2^n.
+        //For arbitrary number, it takes (2^n - 1) - StepOf(y) steps of operation for number 2^n + y.
+         if (n <= 1)
+        return n;
+    int bit = 0;
+    while ((1 << bit) <= n)
+        bit++;
+    return ((1 << bit) - 1) - minimumOneBitOperations(n - (1 << (bit-1)));
+    }
+};
