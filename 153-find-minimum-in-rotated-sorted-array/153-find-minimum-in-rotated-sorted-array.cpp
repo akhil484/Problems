74c61f568e64327c3ad00768b9c99a1e2d86309e
Akhil Sharma <sharma.akhil484@gmail.com>
2022-06-09 21:58:57 +0530

    Time: 7 ms (37.34%), Space: 10.3 MB (23.63%) - LeetHub

--- 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.cpp
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.cpp
@@ -8,8 +8,40 @@ public:
                 return nums[l];
             
             int mid = (l+h)/2;
-            if(nums[mid]>nums[h])
-                l=mid+1;
+            if(nums[mid]>nums[h]){
+//                  # we KNOW the pivot must be to the right of the middle:
+//                 # if nums[mid] > nums[right], we KNOW that the
+//                 # pivot/minimum value must have occurred somewhere to the right
+//                 # of mid, which is why the values wrapped around and became smaller.
+
+//                 # example:  [3,4,5,6,7,8,9,1,2] 
+//                 # in the first iteration, when we start with mid index = 4, right index = 9.
+//                 # if nums[mid] > nums[right], we know that at some point to the right of mid,
+//                 # the pivot must have occurred, which is why the values wrapped around
+//                 # so that nums[right] is less then nums[mid]
+
+//                 # we know that the number at mid is greater than at least
+//                 # one number to the right, so we can use mid + 1 and
+//                 # never consider mid again; we know there is at least
+//                 # one value smaller than it on the right
+                l=mid+1;}
+//             # here, nums[mid] <= nums[right]:
+//                 # we KNOW the pivot must be at mid or to the left of mid:
+//                 # if nums[mid] <= nums[right], we KNOW that the pivot was not encountered
+//                 # to the right of middle, because that means the values would wrap around
+//                 # and become smaller (which is caught in the above if statement).
+//                 # this leaves the possible pivot point to be at index <= mid.
+                            
+//                 # example: [8,9,1,2,3,4,5,6,7]
+//                 # in the first iteration, when we start with mid index = 4, right index = 9.
+//                 # if nums[mid] <= nums[right], we know the numbers continued increasing to
+//                 # the right of mid, so they never reached the pivot and wrapped around.
+//                 # therefore, we know the pivot must be at index <= mid.
+
+//                 # we know that nums[mid] <= nums[right].
+//                 # therefore, we know it is possible for the mid index to store a smaller
+//                 # value than at least one other index in the list (at right), so we do
+//                 # not discard it by doing right = mid - 1. it still might have the minimum value.
             else
                 h=mid;
             
