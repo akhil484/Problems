bc8d5676441d1ee179dbf788ffbee15f0a7cfa4c
Akhil Sharma <sharma.akhil484@gmail.com>
2022-08-17 20:09:41 +0530

    Time: 12 ms (45.69%), Space: 6.1 MB (36.49%) - LeetHub

--- 2376-count-special-integers/2376-count-special-integers.cpp
+++ 2376-count-special-integers/2376-count-special-integers.cpp
@@ -1,5 +1,11 @@
 class Solution {
 public:
+//     mask == 0 means we didn't use any number before
+// i == 0 means we want to use 0 now
+// mask == 0 && i== 0 means if we want to set 0 to current position, as a leading 0
+// for example, if we have 1234,
+// then we can set 0 to first positon, that is 0XXX, 0234, that's 234
+// then we can also set 0 to second position, that is 00XX, 0012, that's 12
     int dp[11][2][1024];
    int util(string &str, int pos = 0, int tight = 1, int mask = 0)
    {
